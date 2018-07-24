9d183f7abe989b2fd8794b434a4d7f98ed711cb8
jalen <jalenparham97@gmail.com>
2018-07-24 08:59:52 -0400

    tryed some new stuff didnt work

--- scripts/script.js
+++ scripts/script.js
@@ -3,32 +3,10 @@
 let userPrompt = prompt("Do you want to play?");
 let grant = 10;
 let user = 40;
-let wins = 0;
-
-// if (userPrompt === "yes" || userPrompt === "Yes") {
-//   let name = prompt(`What is your name?`);
-
-
-//   while (grant <= 10 && user <= 40) {
-//     let randNum = Math.floor(Math.random() * 3);
-//     grant -= randNum;
-//     user -= randNum;
-//     console.log(`Grant has ${grant} health left`);
-//     console.log(`${name} has ${user} health left`);
-
-//     if (grant === 0 || user === 0) {
-//       console.log(`Score is at 0`);
-//       break;
-//     }
-    
-//   }
-// } else {
-//     console.log(`Maybe another time`);
-// }
-
+let loses = 0;
 
 if (userPrompt === "yes" || userPrompt === "Yes") {
-  let name = prompt(`What is your name?`);
+  let name = prompt(`What is your name O Great One?`);
 
 
   while (user > 0) {
@@ -41,8 +19,8 @@ if (userPrompt === "yes" || userPrompt === "Yes") {
 
     if (grant <= 0 ) {
       console.log(`Grant has lost`);
-      wins++
-      if (wins >= 3) {
+      loses++
+      if (loses >= 3) {
         console.log(`${name} is the Victor`)
         break;
       }
@@ -51,12 +29,10 @@ if (userPrompt === "yes" || userPrompt === "Yes") {
     }
     if (user <= 0) {
       console.log(`${name} has lost`);
-    }
-    
+    } 
   }
 } else {
     console.log(`Maybe another time`);
 }
-
 
 
